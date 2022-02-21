f5781af61b03ccb7a5b526835f8e56763705867f
lil-sahil <sahilnagpal93@yahoo.ca>
2022-02-21 12:58:28 -0500

    Basic layout of the daily showcse display complete. Added helper functions to display the temperature values as degrees.

--- src/components/display_today_showcase.js
+++ src/components/display_today_showcase.js
@@ -10,15 +10,27 @@ import { API_CALL } from "./fetch_data";
 
 export const TODAY_SHOWCASE = (() => {
   const controller = (APIdata) => {
+    // At degree symbol to base value
+    const addDegree = (baseString) => {
+      return `${baseString}\xB0C`;
+    };
+
+    // Add percentage Sign
+    const addPercentageSign = (baseString) => {
+      return `${baseString}%`;
+    };
+
     // Remove all previous child elements
     clearChildren(dailyForecastShowcase);
 
     // Get relevant data for the component
-    let currentTemp = APIdata.list[0].main.temp;
-    let feelLikeTemp = APIdata.list[0].main.feels_like;
-    let tempMin = APIdata.list[0].main.temp_min;
-    let tempMax = APIdata.list[0].main.temp_max;
-    let humidity = APIdata.list[0].main.humidity;
+    let currentTemp = addDegree(APIdata.list[0].main.temp.toPrecision(1));
+    let feelLikeTemp = addDegree(
+      APIdata.list[0].main.feels_like.toPrecision(1)
+    );
+    let tempMin = addDegree(APIdata.list[0].main.temp_min.toPrecision(1));
+    let tempMax = addDegree(APIdata.list[0].main.temp_max.toPrecision(1));
+    let humidity = addPercentageSign(APIdata.list[0].main.humidity);
     let weather = APIdata.list[0].weather[0].main;
     let weatherIcon = APIdata.list[0].weather[0].icon;
     let UnixTime = APIdata.list[0].dt;
@@ -47,7 +59,7 @@ export const TODAY_SHOWCASE = (() => {
 
     label.innerHTML = labelString;
 
-    labelString === "Currently"
+    weatherIcon !== undefined
       ? (value.innerHTML = `<img src=${API_CALL.makeAPICallWeather(
           weatherIcon
         )}>`)
@@ -94,7 +106,7 @@ export const TODAY_SHOWCASE = (() => {
 
     // Current Weather
 
-    componentHelper("Currently", (weather = false), mainContainer, weatherIcon);
+    componentHelper(weather, false, mainContainer, weatherIcon);
 
     return mainContainer;
   };
